79c663fb300b9cf1ab460c23dbea61f7717ead69
Johnny Richardson <johnny.c.rich@gmail.com>
2017-02-13 18:48:22 -0500

    Lots of updates and fixes for Android native app wrapper; added build configs and build scripts. Web scripting fixes for use with Android. Setup use of Hockeyapp for mobile test deployments.

--- mobile/android/AtStake/app/src/main/java/emersonengagementlab/edu/atstake/WebActivity.java
+++ mobile/android/AtStake/app/src/main/java/emersonengagementlab/edu/atstake/WebActivity.java
@@ -1,13 +1,85 @@
 package emersonengagementlab.edu.atstake;
 
 import android.app.Activity;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class WebActivity extends Activity {
 
-    String urlString = "http://127.0.0.1:3000/play/mobile";
+    String urlString = "http://app.local:3000/play/mobile";
+    WebView gameWebView;
+
+    public class WebAppInterface {
+
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void webViewResponse(String response) {
+
+            JSONObject jsonResponse = null;
+            try {
+                jsonResponse = new JSONObject(response);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                assert jsonResponse != null;
+                String actionResponse = jsonResponse.getString("action");
+
+                Log.v("WebResponse", actionResponse);
+
+                switch(actionResponse) {
+
+                    // game UI opened
+                    case "join":
+                    case "new":
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                gameWebView.setVisibility(View.VISIBLE);
+                            }
+                        });
+
+
+                        break;
+
+                    default:
+
+                        Log.v("WebResponse", "No handler for action '" + actionResponse + "'");
+
+                        break;
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    void webAction(String actionString) {
+
+        gameWebView.loadUrl("javascript:mobileAction('" + actionString + "')");
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +91,39 @@ public class WebActivity extends Activity {
         if(BuildConfig.ENVIRONMENT == "production")
             urlString = "https://atstakegame.com/play/mobile";
 
-        WebView gameWebView = (WebView) findViewById(R.id.webView);
-        gameWebView.addJavascriptInterface(new WebAppInterface(this), "android");
+        final Button joinGameBtn = (Button) findViewById(R.id.joinGame);
+        final Button newGameBtn = (Button) findViewById(R.id.newGame);
+
+        gameWebView = (WebView) findViewById(R.id.webView);
+
+        gameWebView.getSettings().setJavaScriptEnabled(true);
+        gameWebView.getSettings().setDomStorageEnabled(true);
+        gameWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+        gameWebView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                joinGameBtn.setVisibility(View.VISIBLE);
+                newGameBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
         gameWebView.loadUrl(urlString);
+
+        joinGameBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                webAction("join");
+            }
+        });
+
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                webAction("new");
+            }
+        });
     }
 }
--- mobile/android/AtStake/app/src/main/java/emersonengagementlab/edu/atstake/WebAppInterface.java
+++ mobile/android/AtStake/app/src/main/java/emersonengagementlab/edu/atstake/WebAppInterface.java
@@ -1,21 +1,5 @@
 package emersonengagementlab.edu.atstake;
 
-import android.content.Context;
-import android.webkit.JavascriptInterface;
-
 /**
  * Created by Johnny on 2/10/17.
  */
-public class WebAppInterface {
-
-    Context mContext;
-
-    /** Instantiate the interface and set the context */
-    WebAppInterface(Context c) {
-        mContext = c;
-    }
-
-    @JavascriptInterface
-    public void webViewResponse(String response) {
-    }
-}
