ca4e2e1f5722480047100a46cf3387f19838768d
esalling23 <esalling23@gmail.com>
2017-04-11 19:25:11 -0400

    added animating time to config update small phone layout add limit helper — not yet working

--- templates/helpers/index.js
+++ templates/helpers/index.js
@@ -22,6 +22,17 @@ module.exports = function() {
 
     };
 
+    _helpers.limit = function(ary, max, options) {
+        if(!ary || ary.length == 0)
+            return options.inverse(this);
+
+        var result = [ ];
+        for(var i = 0; i < max && i < ary.length; ++i)
+            result.push(options.fn(ary[i]));
+        
+        return result.join('');
+    };
+
     _helpers.ellipsis = function (limit, currentText) {
      
         if (currentText) 
