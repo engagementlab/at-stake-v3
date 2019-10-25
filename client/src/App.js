8f8a4d1d4c09f61c35cc12b1e66d6b35e961acfb
Johnny Richardson <johnny.c.rich@gmail.com>
2019-10-25 15:31:10 -0400

    Working socket.io on client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import io from 'socket.io-client';
 import './App.css';
 
 import Decks from './components/Decks';
 
-function App() {
-  return (
-    <div className="App">
-      <Decks />
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      response: false
+    };
+  }
+  componentDidMount() {
+    // Try WS connect
+    const socket = io('http://localhost:3001', {path: '/at-stake-socket/', 'reconnection': true,'reconnectionDelay': 500,'maxReconnectionAttempts':Infinity});
+
+    socket.on('connect', ()=> {
+      console.log('connected to socket client')        
+      
+      socket.on('ohhai', () => { 
+        this.setState({ response: 'connected' });
+      });
+
+      socket.emit('hello');
+    });
+    
+  }
+
+  render() {
+    const { response } = this.state;
+      return (
+        <div className="App">
+            {response
+              ? <p>
+                Connected to socket
+              </p>
+              : <p>Loading...</p>}
+        <Decks />
+      </div>
+    );
+  }
+
 }
 
 export default App;
